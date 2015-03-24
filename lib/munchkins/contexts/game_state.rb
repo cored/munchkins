dd6e61940f88794bc4a9b16080cd7f91fbd9818b
cored <george.rafael@gmail.com>
2015-03-24 17:58:18 -0400

    Add move GameState value object to it's own file

---
+++ lib/munchkins/contexts/game_state.rb
@@ -0,0 +1,6 @@
+module Contexts 
+  module Games 
+    class GameState < Struct.new(:message)
+    end
+  end
+end
--- lib/munchkins/policies/games/minimum_amount_of_players.rb
+++ lib/munchkins/policies/games/minimum_amount_of_players.rb
@@ -5,7 +5,7 @@ module Policies
 
       def self.call(game)
         if (game.players.count < MINIMUM_AMOUNT_OF_PLAYERS)
-          Contexts::Games::DealCards::GameState.new("Minimum game players is #{MINIMUM_AMOUNT_OF_PLAYERS}")
+          Contexts::Games::GameState.new("Minimum game players is #{MINIMUM_AMOUNT_OF_PLAYERS}")
         end
       end
     end
